767a29a2ef7c65574f74ebf8fce08e994cac9a22
John Fitzpatrick <j-fitz@rcn.com>
2015-02-27 23:39:14 -0500

    Added new DriveDistance command. Speeds up gently until we get close to the input position. Along the way it checks the difference between the left and rigth encoders and adjusts the voltage accordingly.

--- src/Commands/DriveDistanceCommand.h
+++ src/Commands/DriveDistanceCommand.h
@@ -39,10 +39,22 @@
  */
 class DriveDistanceCommand: public Command {
 private:
-	int distance;
+	float distance; // requested distance in inches
 	bool isCommandDone;
-	float prevLeftEnc,prevRightEnc;
-	float distanceTraveledL;
+	float distanceError;
+	float distanceTravelledL = 0.0;
+	float distanceTravelledR = 0.0;
+	float voltageLeft = 0.0;;
+	float voltageRight = 0.0;;
+	float maxVoltage = 0.9;
+	float timeStep = 0.02;
+	float secondsToMax = 1.5;
+	float voltageStep = maxVoltage/(secondsToMax/timeStep);
+	float wheelDiam = 6.0;
+	float distancePerRev = 3.1416*wheelDiam;
+	float inchesPerTick = distancePerRev/1024;
+	// If we're off by 1 inch, increase the voltage by 0.05
+	float voltageScale = 0.05;
 public:
 	DriveDistanceCommand(int dist);
 	virtual void Initialize();
