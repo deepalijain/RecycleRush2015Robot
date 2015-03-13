b04bc18fd934f565b6ee6ad11c30e3f87501ee9f
Charles Frankston <cbf9427@gmail.com>
2015-03-13 00:06:19 -0400

    Add Joystick2 buttons.
    More autonomous work.
    
    Added Delay command for timing things in Auto.

--- src/Commands/AutonomousCommand1Can.cpp
+++ src/Commands/AutonomousCommand1Can.cpp
@@ -2,8 +2,12 @@
 //
 
 #include "AutonomousCommand1Can.h"
+#include "ToggleFlapsCommand.h"
+
+
 AutonomousCommand1Can::AutonomousCommand1Can() {
 	// Need to go up twice to get a can
+	AddSequential(new ToggleFlapsCommand(-1)); 	// make sure flaps are closed
 	AddSequential(Robot::oi->canUp);
 	//AddSequential(Robot::oi->toteUp);
 	// TODO: DrivePID only goes straight right now. We need to be able to pass in values
